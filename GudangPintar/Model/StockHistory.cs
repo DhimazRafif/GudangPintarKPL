3f663486d6320fe7a91462be9cceb9937ecc0c52
DhimazRafif <hanafidhimaz@gmail.com>
2026-04-13 10:30:54 +0700

    update: pembuatan class model

---
+++ GudangPintar/Model/StockHistory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GudangPintar.Model
+{
+    public class StockHistory
+    {
+        private int historyid;
+        private Stock stock {  get; set; }
+        private int changed_quantity {  get; set; }
+        private User changed_by_user { get; set; }
+        private DateTime change_date { get; set; }
+
+        public StockHistory(Stock stock, int changed_quantity, User changed_by_user, DateTime change_date)
+        {
+            this.stock = stock;
+            this.changed_quantity = changed_quantity;
+            this.changed_by_user = changed_by_user;
+            this.change_date = change_date;
+        }
+    }
+}
---
+++ GudangPintar/Model/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GudangPintar.Model
+{
+    public class User
+    {
+        private int userid; 
+        private string name { get; set; }
+        private string username { get; set; }
+        private string password { get; set; }
+        private Role role { get; set; }
+        private User created_by;
+        private DateTime created_at;
+        private DateTime updated_at;
+
+        public User(int userid, string name, string username, string password, Role role)
+        {
+            this.userid = userid;
+            this.name = name;
+            this.username = username;
+            this.password = password;
+            this.role = role;
+        }
+    }
+
+}
